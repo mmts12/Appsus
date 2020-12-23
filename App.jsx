d64c84feb16a83a1cb423486752d4e9b55fd6671
mmts12 <56490587+mmts12@users.noreply.github.com>
2020-12-23 11:43:55 +0200

    first commit

---
+++ App.jsx
@@ -0,0 +1,28 @@
+import { Home } from './pages/home.jsx';
+
+// Simple React Component
+export function App() {
+  return (
+    <section className="app">
+      <h1>My App</h1>
+      <h2>dsadsa</h2>
+      {/* <Home /> */}
+    </section>
+  );
+}
+
+// Using Class:
+// export class App extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <header>
+//                     <h1>Lets Play</h1>
+//                 </header>
+//                 <main>
+//                     <Home />
+//                 </main>
+//             </div>
+//         )
+//     }
+// }
---
+++ pages/home.jsx
@@ -0,0 +1,13 @@
+export class Home extends React.Component {
+  componentDidMount() {
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <section>
+        <h2>Home Sweet Home</h2>
+      </section>
+    );
+  }
+}
